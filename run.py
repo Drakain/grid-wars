b5bee3e152b9ac20ace103210eae8c9144d803ca
Drakain <daniel.lindholm999@yahoo.com>
2022-11-17 16:24:12 +0000

    Add global variables

--- run.py
+++ run.py
@@ -1,2 +1,22 @@
 import random
 import time
+
+
+# GLOBAL VARIABLES #
+
+# Variable for grid
+grid = [[]]
+# Variable for grid size
+grid_size = 10
+# Variable for number of ships
+ships = 0
+# Variable for ship positions
+ship_positions = [[]]
+# Variable for number of ships sunk
+ships_sunk = 0
+# Variable for bullets left
+bullets = 50
+# Variable for game over
+game_over = False
+# Variable for alphabet
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUWXYZ'
