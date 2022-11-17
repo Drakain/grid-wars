e85cc13642e3a6ae69c60942baaddc40ade81c48
Drakain <daniel.lindholm999@yahoo.com>
2022-11-17 17:01:03 +0000

    Add basic structure of grid-related methods

--- run.py
+++ run.py
@@ -15,8 +15,45 @@ ship_positions = [[]]
 # Variable for number of ships sunk
 ships_sunk = 0
 # Variable for bullets left
-bullets = 50
+bullets = 30
 # Variable for game over
 game_over = False
 # Variable for alphabet
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUWXYZ'
+
+
+def check_grid_and_place_ship(start_row, end_row, start_col, end_col):
+    """
+    Will check grid to see if it is possible to place a ship at the location.
+    """
+
+    global grid
+    global ship_positions
+
+
+def attempt_ship_placement(row, col, direction, length):
+    """
+    Attempts to place a ship on the grid based on direction with help from the above method.
+    """
+
+    global grid_size
+
+
+def generate_grid():
+    """
+    Generates a 10x10 grid and attempts to randomly place down different types of ships at different locations
+    """
+
+    global grid
+    global grid_size
+    global ships
+    global ship_positions
+
+
+def show_grid():
+    """
+    Will show the grid with rows A-J and columns 0-9
+    """
+
+    global grid
+    global alphabet
